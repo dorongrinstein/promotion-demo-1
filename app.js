7fda91dc61552e62a6da29a2edb2c1101f35d209
Doron Grinstein <dorongrinstein@hotmail.com>
2022-03-14 16:04:55 -0700

    demo for mike

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 8080
 
 express()
     .get('/', (req, res) => {
-        res.send('Hello1 - feature b');
+        res.send('Mike is nice');
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
